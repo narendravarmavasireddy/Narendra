d39d8674037d40c159509b486759010216888145
narendravarmavasireddy <38433445+narendravarmavasireddy@users.noreply.github.com>
2025-12-27 22:03:26 +0530

    Update output.tf

--- Terraform/Modules/private_dns/output.tf
+++ Terraform/Modules/private_dns/output.tf
@@ -1,3 +1,12 @@
+output "private_dns_zone_ids" {
+  description = "Map of private DNS zone IDs keyed by zone name"
+  value = {
+    for k, v in azurerm_private_dns_zone.this :
+    k => v.id
+  }
+}
+
+/*
 output "storage_dns_zone_id" {
   value = azurerm_private_dns_zone.storage.id
 }
@@ -9,3 +18,4 @@ output "storage_dns_zone_id" {
 output "keyvault_dns_zone_id" {
   value = azurerm_private_dns_zone.keyvault.id
 }
+*/
