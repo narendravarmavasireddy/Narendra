5d5e56fc2923b1f934f7f7de72ce54c3dfedbe0b
narendravarmavasireddy <38433445+narendravarmavasireddy@users.noreply.github.com>
2025-12-27 22:01:03 +0530

    Update main.tf

--- Terraform/Modules/private_dns/main.tf
+++ Terraform/Modules/private_dns/main.tf
@@ -1,3 +1,21 @@
+resource "azurerm_private_dns_zone" "this" {
+  for_each = var.private_dns_zones
+
+  name                = each.value
+  resource_group_name = var.rg_name
+}
+
+resource "azurerm_private_dns_zone_virtual_network_link" "this" {
+  for_each = var.private_dns_zones
+
+  name                  = "${each.key}-link"
+  resource_group_name   = var.rg_name
+  private_dns_zone_name = azurerm_private_dns_zone.this[each.key].name
+  virtual_network_id   = var.vnet_id
+}
+
+
+/*
 resource "azurerm_private_dns_zone" "storage" {
   name                = "privatelink.blob.core.windows.net"
   resource_group_name = var.rg_name
@@ -33,3 +51,4 @@ resource "azurerm_private_dns_zone_virtual_network_link" "kv_link" {
   private_dns_zone_name = azurerm_private_dns_zone.keyvault.name
   virtual_network_id   = var.vnet_id
 }
+*/
