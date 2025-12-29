67c1dd9208ef2f80ddad9178b10f70301c30a4ff
narendravarmavasireddy <38433445+narendravarmavasireddy@users.noreply.github.com>
2025-12-29 19:11:56 +0530

    Update variables.tf

--- Terraform/variables.tf
+++ Terraform/variables.tf
@@ -15,6 +15,8 @@ variable "storage_account_name" {
 }
 variable "key_vault_name" {}
 
+prefix   = "test-data"
+
 variable "private_endpoints" {
   description = "Private Endpoint configuration map"
   type = map(object({
