c40a801c8a1b2ad62e5bf31b4e2ca8460c07a8a6
narendravarmavasireddy <38433445+narendravarmavasireddy@users.noreply.github.com>
2026-01-02 18:06:38 +0530

    Update backend.tf

--- Terraform/backend.tf
+++ Terraform/backend.tf
@@ -1,8 +1,20 @@
 terraform {
   backend "azurerm" {
     resource_group_name  = "Terraform-Automation"
-    storage_account_name = "terraformdevtestsa" # Must be globally unique
+    storage_account_name = "terraformdevtestsa"
     container_name       = "tfstate"
     key                  = "terraform.infra.tfstate"
   }
+  required_providers {
+    azurerm = {
+      source = "hashicorp/azurerm"
+      version = ">= 3.7.0" # OIDC support added in v3.7.0
+    }
+  }
 }
+
+provider "azurerm" {
+  features {}
+  use_oidc = true
+}
+
