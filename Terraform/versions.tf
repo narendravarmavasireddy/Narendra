548479d0fd02ea613db3280ad6bb166890cf3c68
narendravarmavasireddy <38433445+narendravarmavasireddy@users.noreply.github.com>
2026-01-02 18:12:53 +0530

    Update versions.tf

--- Terraform/versions.tf
+++ Terraform/versions.tf
@@ -4,7 +4,7 @@ terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = ">= 4.0.0"
+      version = ">= 3.7.0"
     }
     azapi = {
       source  = "azure/azapi"
