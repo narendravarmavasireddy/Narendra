d52d916092206a96eef5b4fc18ee7573574fc34b
narendravarmavasireddy <38433445+narendravarmavasireddy@users.noreply.github.com>
2026-01-09 16:07:40 +0530

    Update managedIdentity.tf

--- Terraform/managedIdentity.tf
+++ Terraform/managedIdentity.tf
@@ -1,15 +1,17 @@
-data "azurerm_resources" "databricks_mrg_identities" {
-  resource_group_name = module.databricks.managed_resource_group_name
-  type                = "Microsoft.ManagedIdentity/userAssignedIdentities"
+data "azapi_resource" "databricks_mrg_identity" {
+  type      = "Microsoft.ManagedIdentity/userAssignedIdentities@2023-01-31"
+  name      = "dbmanagedidentity"
+  parent_id = "/subscriptions/${data.azurerm_client_config.current.subscription_id}/resourceGroups/${module.databricks.managed_resource_group_name}"
 
   depends_on = [
     module.databricks
   ]
 }
 
+
 locals {
-  databricks_mrg_identity = one([
-    for r in data.azurerm_resources.databricks_mrg_identities.resources :
-    r if r.name == "dbmanagedidentity"
-  ])
+  databricks_mrg_principal_id = jsondecode(
+    data.azapi_resource.databricks_mrg_identity.output
+  ).properties.principalId
 }
+
