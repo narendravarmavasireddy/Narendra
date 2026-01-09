e1a03c74dbd4707dadfaaf1d89b20521a264973b
narendravarmavasireddy <38433445+narendravarmavasireddy@users.noreply.github.com>
2026-01-09 15:01:57 +0530

    Update managedIdentity.tf

--- Terraform/managedIdentity.tf
+++ Terraform/managedIdentity.tf
@@ -1,4 +1,9 @@
-data "azurerm_user_assigned_identity" "databricks_mrg_identity" {
-  name                = "dbmanagedidentity"
+data "azurerm_resources" "databricks_mrg_identities" {
   resource_group_name = module.databricks.managed_resource_group_name
+  type                = "Microsoft.ManagedIdentity/userAssignedIdentities"
+
+  depends_on = [
+    module.databricks
+  ]
 }
+
