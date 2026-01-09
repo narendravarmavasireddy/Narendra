0b193ce0f73c73f3bb6204dd4b5217cdc35c0d17
narendravarmavasireddy <38433445+narendravarmavasireddy@users.noreply.github.com>
2026-01-09 12:46:13 +0530

    Update output.tf

--- Terraform/Modules/databricks/output.tf
+++ Terraform/Modules/databricks/output.tf
@@ -1,7 +1,6 @@
 output "workspace_id" {
   value = azurerm_databricks_workspace.workspace.id
 }
-output "managed_identity_principal_id" {
-  description = "Principal ID of Databricks system-assigned managed identity"
-  value       = azurerm_databricks_workspace.workspace.identity[0].principal_id
+output "access_connector_principal_id" {
+  value = azurerm_databricks_access_connector.connector.identity[0].principal_id
 }
