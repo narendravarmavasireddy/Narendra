cc25af0b39eefa520f8798f73de1f21a2a26ba89
narendravarmavasireddy <38433445+narendravarmavasireddy@users.noreply.github.com>
2026-01-12 22:50:10 +0530

    Update outputs.tf

--- Terraform/outputs.tf
+++ Terraform/outputs.tf
@@ -1,3 +1,8 @@
 output "databricks_workspace_id" {
   value = module.databricks.workspace_id
 }
+output "databricks_mrg_principal_id" {
+  description = "Principal ID of Databricks managed identity in managed resource group"
+  value       = local.databricks_mrg_principal_id
+}
+
