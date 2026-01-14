d4287b896764eb5edd62df1924b656fee7c7b4a4
narendravarmavasireddy <38433445+narendravarmavasireddy@users.noreply.github.com>
2026-01-14 17:22:20 +0530

    Create common.go

---
+++ Terratest/Test/common.go
@@ -0,0 +1,14 @@
+package test
+
+import (
+    "testing"
+
+    "github.com/gruntwork-io/terratest/modules/terraform"
+)
+
+func terraformOptions(t *testing.T) *terraform.Options {
+    return &terraform.Options{
+        TerraformDir: "../Terraform/envs/Test",
+        NoColor:      true,
+    }
+}
