01e4b80b192b9deae6899b7d62f51e3c71f429f4
narendravarmavasireddy <38433445+narendravarmavasireddy@users.noreply.github.com>
2026-01-14 22:08:49 +0530

    Update outputs.tf

--- Terraform/outputs.tf
+++ Terraform/outputs.tf
@@ -48,6 +48,5 @@ output "storage_account_id" {
   value = module.storage.storage_id
 }
 output "databricks_managed_resource_group_name" {
-  description = "Databricks managed resource group name"
   value       = module.databricks.managed_resource_group_name
 }
